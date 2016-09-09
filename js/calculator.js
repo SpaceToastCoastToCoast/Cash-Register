aa1c851e1e6c69c375aa04dda18d1baad0a845df
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-08 20:01:47 -1000

    setting up html

---
+++ js/calculator.js
@@ -0,0 +1,70 @@
+function Calculator() {
+  let _memory;
+  let _total;
+
+  let calculator = {};
+
+  calculator.load = function(x) {
+    this._total = x;
+    if(typeof this._total !== 'number') {
+      throw new Error('Type mismatch');
+    }
+    return this._total;
+  };
+
+  calculator.getTotal = function() {
+    if(this._total === undefined) {
+      this._total = 0;
+    }
+    return this._total;
+  };
+
+  calculator.add = function (x) {
+    if(typeof x === 'number') {
+      this._total += x;
+    } else {
+      throw new Error('Type mismatch');
+    }
+  };
+
+  calculator.subtract = function (x) {
+    if(typeof x === 'number') {
+      this._total -= x;
+    } else {
+      throw new Error('Type mismatch');
+    }
+  };
+
+  calculator.multiply = function (x) {
+    if(typeof x === 'number') {
+      this._total *= x;
+    } else {
+      throw new Error('Type mismatch');
+    }
+  };
+
+  calculator.divide = function (x) {
+    if(typeof x === 'number') {
+      this._total /= x;
+    } else {
+      throw new Error('Type mismatch');
+    }
+  };
+
+  calculator.recallMemory = function () {
+    if(typeof this._memory !== 'number') {
+      this._memory = 0;
+    }
+    return this._memory;
+  };
+
+  calculator.saveMemory = function() {
+    this._memory = this._total;
+  };
+
+  calculator.clearMemory = function () {
+    this._memory = 0;
+  };
+
+  return calculator;
+}
---
+++ js/cash_register.js
@@ -0,0 +1,73 @@
+function CashRegister() {
+  let cashRegister = {};
+  let buffer = [];
+  let balance = 0;
+
+  //give the cash register a calculator
+  cashRegister.calc = Calculator();
+
+  //add each number pressed to a buffer
+  cashRegister.bufferInput = function (input) {
+    if(typeof input === 'number') {
+      buffer.push(input);
+    }
+  };
+
+  //converts buffer of numbers in the display to a usable format
+  cashRegister.convertInput = function() {
+    return parseInt(buffer.join(""));
+  };
+
+  //load the value in the buffer into the calculator
+  cashRegister.load = function() {
+    this.calc.load(this.convertInput());
+  };
+
+  //add whatever is in our buffer to the calculator's total
+  cashRegister.add = function() {
+    this.calc.add(this.convertInput());
+  };
+
+  //subtract whatever is in our buffer to the calculator's total
+  cashRegister.subtract = function() {
+    this.calc.subtract(this.convertInput());
+  };
+
+  cashRegister.equals = function() {
+    this.updateDisplayByInput(this.calc.getTotal());
+  };
+
+  //write what's in the display to the register's balance
+  cashRegister.depositCash = function() {
+    balance += parseInt(document.getElementById('display').innerHTML);
+  };
+
+  cashRegister.withdrawCash = function() {
+    balance -= parseInt(document.getElementById('display').innerHTML);
+  };
+
+  //write what's in the buffer to the display
+  cashRegister.updateDisplay = function() {
+    //'$' + toFixed()
+    document.getElementById('display').innerHTML = ('$' + this.convertInput().toFixed());
+  };
+
+  //write what's in a value to the display (overloaded)
+  cashRegister.updateDisplayByInput = function(input) {
+    //'$' + toFixed()
+    document.getElementById('display').innerHTML = ('$' + input.toFixed());
+  };
+
+  //set display to 0
+  cashRegister.clearDisplay = function() {
+    document.getElementById('display').innerHTML = '$0.00';
+  };
+
+  return cashRegister;
+}
+
+var reg = CashRegister();
+setInterval(reg.updateDisplay(), 1000);
+
+document.getElementById("add").addEventListener("click", reg.add);
+document.getElementById("subtract").addEventListener("click", reg.subtract);
